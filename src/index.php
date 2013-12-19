dfbedd569319090e46408c7b8e6a71e6da4c9cc5
baba5246 <baba_writer@yahoo.co.jp>
2013-12-20 01:38:05 +0900

    土台作成

---
+++ src/index.php
@@ -0,0 +1,28 @@
+<?php
+include_once('signed_request.php');
+require 'facebook.php';
+//●●はさきほどメモしたID等を入れてください
+$facebook = new Facebook(array('appId' => '●●●●●●●●●●',
+                              'secret' => '●●●●●●●●●●',
+                              'cookie' => true,
+                        ));
+$fb_user = $facebook->getUser();
+//いいねを押しているかどうか判別
+  if ( isset($_POST['signed_request']) ) {
+    $fb_data = parse_signed_request($_POST['signed_request'], '●●●●●●●●●●');
+    if( $fb_data['page']['liked'] ){
+                //いいねとは別にアプリの権限認証をしているかどうか確認する
+                //されていなければ、権限認証画面へ転送する
+        if (!$fb_user) {
+                //scopeの部分はどこまで権限がほしいか任意設定できる
+        $par = array('scope' => 'publish_stream','redirect_uri' => 'リダイレクトURL。今回はfacebookページのアドレス。');
+        $fb_login_url = $facebook->getLoginUrl($par);
+                //javascriptで転送させないと、間にfacebookのロゴが出てくる減少が起こる
+        echo "<script type='text/javascript'>top.location.href = '$fb_login_url';</script>";
+        }
+      include_once('fan.php');
+    } else {
+      include_once('notfan.php');
+    }
+  }
+?>
